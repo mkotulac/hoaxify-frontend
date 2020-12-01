440375d92e5ba35e59f922d8545eb5efbd53845b
mkotulac <martin.kotulac@gmail.com>
2020-12-01 21:44:16 +0100

    add styles

--- src/pages/UserSignupPage.js
+++ src/pages/UserSignupPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class UserSignupPage extends React.Component {
 
@@ -40,40 +41,50 @@ class UserSignupPage extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Sign Up</h1>
-                <div>
+            <div className={'container'}>
+                <h1 className='text-center'>Sign Up</h1>
+                <div className='col-12 mb-3'>
+                    <label>Display name</label>
                     <input
+                        className='form-control'
                         placeholder={'Your display name'}
                         value={this.state.displayName}
                         onChange={this.onChangeDisplayName}
                     />
                 </div>
-                <div>
+                <div className='col-12 mb-3'>
+                    <label>Username</label>
                     <input
+                        className='form-control'
                         placeholder={'Your username'}
                         value={this.state.username}
                         onChange={this.onChangeUsername}
                     />
                 </div>
-                <div>
+                <div className='col-12 mb-3'>
+                    <label>Password</label>
                     <input
+                        className='form-control'
                         type={'password'}
                         placeholder={'Your password'}
                         value={this.state.password}
                         onChange={this.onChangePassword}
                     />
                 </div>
-                <div>
+                <div className='col-12 mb-3'>
+                    <label>Repeat password</label>
                     <input
+                        className='form-control'
                         type={'password'}
                         placeholder={'Repeat your password'}
                         value={this.state.passwordRepeat}
                         onChange={this.onChangePasswordRepeat}
                     />
                 </div>
-                <div>
+                <div
+                className='text-center'>
                     <button
+                        className='btn btn-primary'
                         onClick={this.onClickSignup}
                     >
                         Sign Up
